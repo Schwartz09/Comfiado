d0c0fcc0d5d4fda6d62cf651457ac742b3c75c86
unknown <henriquemaliaoliveira@gmail.com>
2022-10-03 15:25:35 -0300

    optimize imports

--- app/src/main/java/br/edu/qi/comfiado/Auth.java
+++ app/src/main/java/br/edu/qi/comfiado/Auth.java
@@ -1,12 +1,5 @@
 package br.edu.qi.comfiado;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
