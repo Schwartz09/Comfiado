6805330da9ba08e83883725110d9e17509acb46e
unknown <henriquemaliaoliveira@gmail.com>
2022-10-03 15:03:32 -0300

    first commit

---
+++ app/src/main/java/br/edu/qi/comfiado/banco/UsuarioDAO.java
@@ -0,0 +1,5 @@
+package br.edu.qi.comfiado.banco;
+
+public class UsuarioDAO {
+
+}
---
+++ app/src/main/java/br/edu/qi/comfiado/modelo/Divida.java
@@ -0,0 +1,73 @@
+package br.edu.qi.comfiado.modelo;
+
+import java.util.Calendar;
+
+public class Divida {
+    private int id;
+
+    private int idCredor;
+    private int idDevedor;
+
+    private float valor;
+
+    private String descricao;
+
+    private Calendar dataAbertura;
+    private Calendar dataPagamento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCredor() {
+        return idCredor;
+    }
+
+    public void setIdCredor(int idCredor) {
+        this.idCredor = idCredor;
+    }
+
+    public int getIdDevedor() {
+        return idDevedor;
+    }
+
+    public void setIdDevedor(int idDevedor) {
+        this.idDevedor = idDevedor;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Calendar getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(Calendar dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public Calendar getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Calendar dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+}
