96be833f718f3f73470af4e1ef92282061ddcf3d
unknown <henriquemaliaoliveira@gmail.com>
2022-10-05 16:41:37 -0300

    implemented login and register

--- app/src/main/java/br/edu/qi/comfiado/CadastroActivity.java
+++ app/src/main/java/br/edu/qi/comfiado/CadastroActivity.java
@@ -1,26 +1,47 @@
 package br.edu.qi.comfiado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import br.edu.qi.comfiado.modelo.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private EditText edtEmail;
+    private EditText edtSenha;
 
     private Button btnCadastro;
     private Button btnLogin;
 
-    // TODO: Criar atributos edtText para os campos do cadastro
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        this.mAuth = FirebaseAuth.getInstance();
+        this.mDatabase = FirebaseDatabase.getInstance().getReference().child("usuarios");
+
+        // TODO: implementar a tela de cadastro utilizando os ids abaixo
+
+        this.edtEmail = findViewById(R.id.edtEmail);
+        this.edtSenha = findViewById(R.id.edtSenha);
 
         this.btnCadastro = findViewById(R.id.btnCadastro);
         this.btnLogin = findViewById(R.id.btnLogin);
@@ -48,14 +69,41 @@ public class CadastroActivity extends AppCompatActivity {
         finish();
     }
 
-    private void trocarParaActivityPrincipal(View v) {
+    private void trocarParaActivityPrincipal() {
         Intent i = new Intent(CadastroActivity.this, MainActivity.class);
         startActivity(i);
         finish();
     }
 
     public void cadastrar() {
-        // TODO: Implementar cadastro pelo firebase
-        // TODO: Se o cadastro for bem sucedido, troca pra activity principal.
+
+        String email = this.edtEmail.getText().toString();
+        String senha = this.edtSenha.getText().toString();
+
+        // TODO: buscar outros atributos
+
+        Usuario cadastro = new Usuario();
+
+        cadastro.setEmail(email);
+        cadastro.setSenha(senha);
+
+        // TODO: definir outros atributos
+
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String uid = task.getResult().getUser().getUid();
+
+                            mDatabase.child(uid).setValue(cadastro);
+
+                            Toast.makeText(CadastroActivity.this, "Bem vindo", Toast.LENGTH_SHORT).show();
+                            trocarParaActivityPrincipal();
+                        } else {
+                            Toast.makeText(CadastroActivity.this, "Houve um problema no cadastro", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     };
 }
--- app/src/main/java/br/edu/qi/comfiado/LoginActivity.java
+++ app/src/main/java/br/edu/qi/comfiado/LoginActivity.java
@@ -1,12 +1,21 @@
 package br.edu.qi.comfiado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtSenha;
     private Button btnLogin;
     private Button btnCadastro;
+
+    private FirebaseAuth mAuth;
 
     // TODO: implementar lembre-me
 
@@ -23,39 +34,49 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        this.mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            this.trocarParaActivityPrincipal();
+        }
+
+        // TODO: implementar a tela de login com os ids abaixo
+
         this.edtEmail = findViewById(R.id.edtEmail);
         this.edtSenha = findViewById(R.id.edtSenha);
+
         this.btnLogin = findViewById(R.id.btnLogin);
         this.btnCadastro = findViewById(R.id.btnCadastro);
 
         this.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login(view);
+                login();
             }
         });
 
         this.btnCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                trocarParaActivityCadastro(view);
+                trocarParaActivityCadastro();
             }
         });
     }
 
-    private void trocarParaActivityCadastro(View v) {
+    private void trocarParaActivityCadastro() {
         Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(i);
         finish();
     }
 
-    private void trocarParaActivityPrincipal(View v) {
+    private void trocarParaActivityPrincipal() {
         Intent i = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(i);
         finish();
     }
 
-    private void login(View v) {
+    private void login() {
         String email = this.edtEmail.getText().toString();
         String senha = this.edtSenha.getText().toString();
 
@@ -67,9 +88,18 @@ public class LoginActivity extends AppCompatActivity {
             this.edtSenha.setError("Insira sua senha");
         }
 
-        // TODO: Implementar login com o firebase
-
-        // TODO: Se o login tiver sucesso, troca pra activity principal
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Bem vindo", Toast.LENGTH_SHORT).show();
+                            trocarParaActivityPrincipal();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login ou Email INCORRETO", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
     }
