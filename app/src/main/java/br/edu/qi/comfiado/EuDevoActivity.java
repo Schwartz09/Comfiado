749a1896687c3958f101751f1353f5f4d49214c3
unknown <henriquemaliaoliveira@gmail.com>
2022-10-15 10:59:43 -0300

    add view activities

---
+++ app/src/main/java/br/edu/qi/comfiado/EuDevoActivity.java
@@ -0,0 +1,14 @@
+package br.edu.qi.comfiado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EuDevoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eu_devo);
+    }
+}
---
+++ app/src/main/java/br/edu/qi/comfiado/MeDevemActivity.java
@@ -0,0 +1,14 @@
+package br.edu.qi.comfiado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeDevemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_devem);
+    }
+}
